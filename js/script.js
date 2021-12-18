c3200da944061b4927a00c34e316a0ed06f21561
SanteriP <santeri.passi@gmail.com>
2021-12-18 03:20:29 +0200

    Add game logic, lose state and win state

--- js/script.js
+++ js/script.js
@@ -1,10 +1,55 @@
 function computerPlay() {
     let roll = Math.random();
     if (roll < 1/3) {
-        return "Rock";
+        return "rock";
     } else if (roll < 2/3) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You lose! Paper beats rock!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win! Rock beats scissors!";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You lose! Scissors beats rock!";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win! Paper beats rock!";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You lose! Rock beats scissors!";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win! Scissors beats paper!";
+    }
+}
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    while(playerScore < 5 && computerScore < 5) {
+        let playerSelection = prompt("Rock, papers or scissors?").toLowerCase();
+        let result = playRound(playerSelection,computerPlay());
+        if(result.includes("win")) {
+            playerScore += 1;
+            console.log(result);
+            console.log(`The score is: Player ${playerScore} | ${computerScore} Computer`);
+        } else if (result.includes("lose")) {
+            computerScore += 1;
+            console.log(result);
+            console.log(`The score is: Player ${playerScore} | ${computerScore} Computer`);
+        } else {
+            console.log(result);
+            console.log(`The score is: Player ${playerScore} | ${computerScore} Computer`);
+        }
+    }
+
+    if(playerScore === 5) {
+        console.log("Congratulation, you won the game!");
+    } else {
+        console.log("You lost the game! Try again!");
     }
 }
